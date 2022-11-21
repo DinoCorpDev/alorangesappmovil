4b57c760100046bc015f8bc4aada6ff0e2f06caf
Jhon Michael Garcia <me@maikoldev.com>
2022-11-21 10:46:45 -0500

    Se crea la pagina TestComponets, para hacer pruebas

--- resources/js/pages/TestComponents.vue
+++ resources/js/pages/TestComponents.vue
@@ -1,54 +1,50 @@
 <template>
-    <div class=fondo>
-        <custom-button 
-        text="Ir a tienda"   
-        variant="#E2E6EA"
-        icon="la-store-alt"
-        class="black--text"
-    ></custom-button>
-    <custom-button 
-        text="Comunicarse con ventas"
-        variant="#F8F9FA"
-        outlined="outlined"   
-    ></custom-button>
+  <v-container class="py-16">
+    <v-row>
+      <v-col cols="12">
+        <div class="mb-4">
+          <h2>Botones</h2>
+          <custom-button
+            text="Ir a tienda"
+            variant="light"
+            icon="la-store-alt"
+            class="black--text"
+          ></custom-button>
+          <custom-button
+            text="Comunicarse con ventas"
+            variant="light"
+            outlined
+          ></custom-button>
+        </div>
 
-    <carousel no-dots :NoArrows="true"
-    >   
-    </carousel>
+        <div class="mb-4">
+          <h2>Carousel</h2>
+          <carousel no-dots :NoArrows="true"> </carousel>
+        </div>
 
-    <div class="prueba">
-        <item-product
+        <div class="mb-4">
+          <h2>Item Producto</h2>
+          <item-product
             title="Producto 1"
             descripcion="Ad nulla magna labore dolore cupidatat sit. Officia in commodo excepteur dolore consequat tempor consequat adipisicing id qui ullamco eu tempor. Quis enim velit non mollit deserunt. Et non esse do occaecat cupidatat. Non adipisicing ullamco est sit velit laborum fugiat et occaecat ipsum ut et. Amet aliqua laboris cillum adipisicing dolor velit ad sint nisi aute culpa."
             idItem="1"
             textColor="white--text"
             colorCard="#23272B"
             img="/public/assets/img/idovela-banner.png"
-        ></item-product>
+          ></item-product>
+        </div>
 
-        <item-product
-            title="Producto 1"
+        <div class="mb-4">
+          <h2>Banner Categoria</h2>
+          <banner-category-product
+            title="Titulo"
             descripcion="Ad nulla magna labore dolore cupidatat sit. Officia in commodo excepteur dolore consequat tempor consequat adipisicing id qui ullamco eu tempor. Quis enim velit non mollit deserunt. Et non esse do occaecat cupidatat. Non adipisicing ullamco est sit velit laborum fugiat et occaecat ipsum ut et. Amet aliqua laboris cillum adipisicing dolor velit ad sint nisi aute culpa."
-            idItem="1"
-            textColor="white--text"
-            colorCard="#23272B"
-        ></item-product>
-
-        <item-product
-            title="Producto 1"
-            descripcion="Ad nulla magna labore dolore cupidatat sit. Officia in commodo excepteur dolore consequat tempor consequat adipisicing id qui ullamco eu tempor. Quis enim velit non mollit deserunt. Et non esse do occaecat cupidatat. Non adipisicing ullamco est sit velit laborum fugiat et occaecat ipsum ut et. Amet aliqua laboris cillum adipisicing dolor velit ad sint nisi aute culpa."
-            idItem="1"
-            textColor="white--text"
-            colorCard="#23272B"
-        ></item-product>
-    </div>
-    <banner-category-product
-        title="Titulo"
-        descripcion="Ad nulla magna labore dolore cupidatat sit. Officia in commodo excepteur dolore consequat tempor consequat adipisicing id qui ullamco eu tempor. Quis enim velit non mollit deserunt. Et non esse do occaecat cupidatat. Non adipisicing ullamco est sit velit laborum fugiat et occaecat ipsum ut et. Amet aliqua laboris cillum adipisicing dolor velit ad sint nisi aute culpa."
-        idItem="100"
-    >
-    </banner-category-product>
-    </div>
+            idItem="100"
+          ></banner-category-product>
+        </div>
+      </v-col>
+    </v-row>
+  </v-container>
 </template>
 
 <script>
@@ -57,25 +53,20 @@ import Carousel from "../components/global/Carousel.vue";
 import CustomButton from "../components/global/CustomButton.vue";
 import ItemProduct from "../components/global/ItemProduct.vue";
 export default {
-    components: {
+  components: {
     BannerCategoryProduct,
     Carousel,
     CustomButton,
     ItemProduct,
   },
-}
+};
 </script>
 
 <style>
-    .fondo{
-        background-color: black;
-        width: 100%;
-        height: 100%;
-    }
-    .prueba
-    {
-        display: flex;
-        padding: 20px;
-    }
-
+.v-main {
+  background-color: #000000;
+}
+h2 {
+  color: #ffffff;
+}
 </style>
