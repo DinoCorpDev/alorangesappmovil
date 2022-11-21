51796bbbc122e1d890bae1a68eee18356441dda9
Jhon Michael Garcia <me@maikoldev.com>
2022-11-21 10:48:37 -0500

    Correciones en el CustomButton

--- resources/js/components/global/CustomButton.vue
+++ resources/js/components/global/CustomButton.vue
@@ -1,64 +1,87 @@
 <template>
-    <v-btn
-        v-bind:color="variant"
-        class="text-uppercase custom-button"
-        v-bind:outlined="outlined"
-        v-bind:disabled="disabled"
-        v-bind:small="small"
-        v-bind:large="large"
-        v-bind:block="block"
-    >
-        <i :class="`las ${icon}`" :style="`font-size: ${sizeIcon}; ${stylesIcon}`" v-if="icon"></i>
-        {{ text }}
-    </v-btn>
+  <v-btn
+    class="text-uppercase custom-button"
+    :color="variant"
+    :outlined="outlined"
+    :disabled="disabled"
+    :small="small"
+    :large="large"
+    :block="block"
+  >
+    <i
+      v-if="icon"
+      :class="`las ${icon}`"
+      :style="`font-size: ${sizeIcon}; ${stylesIcon}`"
+    ></i>
+    <span>{{ text }}</span>
+  </v-btn>
 </template>
 
 <script>
 export default {
-    name: "CustomButton",
-    props: {
-        text:String,
-        icon: {
-            type:String,
-            
-        },
-        variant: {
-            type:String,
-            default: "#white"
-        },
-        outlined:String,
-        disabled:String,
-        small:String,
-        large:String,
-        block:String,
-        ml:String,
-        sizeIcon: {
-            type:String,
-            default: "20px"
-        },
-        stylesIcon: {
-            type:String,
-            default: "margin-right: 10px;"
-        }
-
+  name: "CustomButton",
+  props: {
+    text: String,
+    icon: {
+      type: String,
     },
+    variant: {
+      type: String,
+      default: "light",
+    },
+    outlined: {
+      type: Boolean,
+      default: false,
+    },
+    disabled: {
+      type: Boolean,
+      default: false,
+    },
+    small: String,
+    large: String,
+    block: String,
+    ml: String,
+    sizeIcon: {
+      type: String,
+      default: "20px",
+    },
+    stylesIcon: {
+      type: String,
+      default: "margin-right: 10px;",
+    },
+  },
 };
 </script>
 
-<style lang="scss">
-    .theme--light.v-btn{
-        &.custom-button:hover{
-            background-color: #F8F9FA !important;
-            
-            &:before{
-                background-color: #F8F9FA !important;
-            }
-        }
-    }
-    .theme--light.v-btn--outlined{
-        &.custom-button:hover{
-            color:black !important;
-        }
-    }
-    
+<style lang="scss" scoped>
+// .theme--light.v-btn.light:hover {
+//   background-color: #e2e6ea;
+
+//   &:before {
+//     background-color: #e2e6ea !important;
+//     opacity: 1;
+//   }
+// }
+// .theme--light.v-btn--outlined {
+//   &.custom-button:hover {
+//     color: black !important;
+//   }
+// }
+
+// -------------------
+
+// .theme--light.v-btn {
+//   &:before {
+//     opacity: 1 !important;
+//     background-color: unset;
+//   }
+
+//   &:hover:before {
+//     background-color: #e2e6ea;
+//   }
+
+//   &.v-btn--disabled.v-btn--has-bg {
+//     background-color: #fafcfc !important;
+//   }
+// }
 </style>
