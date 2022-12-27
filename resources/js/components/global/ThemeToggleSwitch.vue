3bb858a13996e8898c10a1d93785416c47330afb
Jhon Michael Garcia <me@maikoldev.com>
2022-12-26 22:41:45 -0500

    ThemeToggleSwitch component added

---
+++ resources/js/components/global/ThemeToggleSwitch.vue
@@ -0,0 +1,14 @@
+<template>
+  <v-btn @click="toggleTheme()">🌛</v-btn>
+</template>
+
+<script>
+export default {
+  name: "ThemeToggleSwitch",
+  methods: {
+    toggleTheme() {
+      this.$vuetify.theme.dark = !this.$vuetify.theme.dark;
+    }
+  }
+};
+</script>
