e25c453b736872b1e3c683ac7dbd17db5079ea0f
Jhon Michael Garcia <me@maikoldev.com>
2023-01-19 11:28:24 -0500

    NavbarBottomNav component added

---
+++ resources/js/components/global/NabvarBottomBar.vue
@@ -0,0 +1,80 @@
+<template>
+    <v-container class="bar" fluid>
+        <router-link class="bar-item" :to="{ name: backTo }">
+            <back-arrow-icon />
+            <span class="bar-label ml-3">{{ text1 }}</span>
+        </router-link>
+        <div class="bar-item">
+            <span class="bar-label mr-3">{{ text2 }}</span>
+            <pad-lock-icon />
+        </div>
+    </v-container>
+</template>
+
+<script>
+import BackArrowIcon from "../icons/BackArrow.vue";
+import PadLockIcon from "../icons/PadLock.vue";
+
+export default {
+    name: "NabvarBottomBar",
+    props: {
+        text1: {
+            type: String,
+            default: "Volver"
+        },
+        text2: {
+            type: String,
+            default: "Seguro"
+        },
+        icon1: {
+            type: String,
+            default: "la-arrow-left"
+        },
+        icon2: {
+            type: String,
+            default: "la-lock"
+        },
+        backTo: {
+            type: String
+        }
+    },
+    components: {
+        BackArrowIcon,
+        PadLockIcon
+    }
+};
+</script>
+
+<style lang="scss" scoped>
+.bar {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+
+    &-item {
+        display: flex;
+        align-items: center;
+
+        &::v-deep {
+            svg {
+                path {
+                    fill: #040405;
+                }
+            }
+        }
+    }
+
+    &-label {
+        text-transform: uppercase;
+        font-size: 12px;
+        font-weight: 700;
+        letter-spacing: 0.15px;
+        color: #000000;
+        margin-top: 3px;
+
+        @media (min-width: 600px) {
+            font-size: 16px;
+        }
+    }
+}
+</style>
---
+++ resources/js/components/icons/BackArrow.vue
@@ -0,0 +1,10 @@
+<template>
+    <svg xmlns="http://www.w3.org/2000/svg" width="15.029" height="12.753" viewBox="0 0 15.029 12.753">
+        <g transform="translate(0 0)">
+            <path
+                d="M79.058,15.858l5.314-5.314,1.063,1.063-4.561,4.561H93.025v1.5l-12.154,0,4.563,4.563L84.372,23.3l-5.314-5.314L78,16.921Z"
+                transform="translate(-77.996 -10.544)"
+            />
+        </g>
+    </svg>
+</template>
