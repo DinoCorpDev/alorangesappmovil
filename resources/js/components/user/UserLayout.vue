5104d4c0168e2f4cd62f54cd7a0f1a84792bbfd0
Jhon Michael Garcia <me@maikoldev.com>
2023-01-20 19:16:43 -0500

    UserLayout container added

--- resources/js/components/user/UserLayout.vue
+++ resources/js/components/user/UserLayout.vue
@@ -1,27 +1,26 @@
 <template>
-    <div class="">
+    <div>
         <layout-navbar-auth />
-        <nabvar-bottom-bar />
-        <v-row no-gutters>
-            <v-col lg="3" class="d-none d-lg-block">
-                <sidemenu></sidemenu>
-            </v-col>
-            <v-col cols="12" lg="9" class="pt-0">
-                <router-view></router-view>
-            </v-col>
-        </v-row>
+        <v-container fluid>
+            <v-row no-gutters>
+                <v-col lg="3" class="d-none d-lg-block">
+                    <sidemenu></sidemenu>
+                </v-col>
+                <v-col cols="12" lg="9" class="pt-0">
+                    <router-view></router-view>
+                </v-col>
+            </v-row>
+        </v-container>
     </div>
 </template>
 
 <script>
-import sidemenu from "./SideMenu";
-import NabvarBottomBar from "../../components/global/NabvarBottomBar.vue";
+import SideMenu from "./SideMenu";
 import LayoutNavbarAuth from "../../components/global/LayoutNavbarAuth.vue";
 
 export default {
     components: {
-        sidemenu,
-        NabvarBottomBar,
+        SideMenu,
         LayoutNavbarAuth
     }
 };
