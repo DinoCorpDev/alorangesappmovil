1d6c0d4a3d7db1fc4decb5e8e925fcd4cdc003e3
Jhon Michael Garcia <me@maikoldev.com>
2023-01-30 11:57:30 -0500

    Limpieza

--- app/Models/Brand.php
+++ app/Models/Brand.php
@@ -7,20 +7,19 @@ use App;
 
 class Brand extends Model
 {
+    protected $fillable = ['name'];
 
-  protected $fillable = ['name'];
+    protected $with = ['brand_translations'];
 
-  protected $with = ['brand_translations'];
+    public function getTranslation($field = '', $lang = false)
+    {
+        $lang = $lang == false ? App::getLocale() : $lang;
+        $brand_translation = $this->brand_translations->where('lang', $lang)->first();
+        return $brand_translation != null ? $brand_translation->$field : $this->$field;
+    }
 
-  public function getTranslation($field = '', $lang = false)
-  {
-    $lang = $lang == false ? App::getLocale() : $lang;
-    $brand_translation = $this->brand_translations->where('lang', $lang)->first();
-    return $brand_translation != null ? $brand_translation->$field : $this->$field;
-  }
-
-  public function brand_translations()
-  {
-    return $this->hasMany(BrandTranslation::class);
-  }
+    public function brand_translations()
+    {
+        return $this->hasMany(BrandTranslation::class);
+    }
 }
