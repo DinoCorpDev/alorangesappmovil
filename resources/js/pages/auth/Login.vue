be4bfb4d910e6a7e9b49b7b98d299b0f440a731f
Jhon Michael Garcia <me@maikoldev.com>
2023-03-10 14:59:59 -0500

    Old auth routes replaced for the new ones

--- resources/js/pages/auth/Login.vue
+++ resources/js/pages/auth/Login.vue
@@ -1,9 +1,69 @@
 <template>
-    <div>
-        <v-container>
-            <v-row>
-                <v-col xl="10" class="mx-auto my-5 my-lg-16 ">
-                    <login-form />
+    <div class="login d-flex flex-column h-100">
+        <layout-navbar-auth />
+        <v-container class="flex-grow-1 mb-5">
+            <v-row class="wrap pa-5" no-gutters>
+                <v-col cols="12" lg="6">
+                    <div class="d-lg-none">
+                        <h1 class="login-title">Inicio de sesión</h1>
+                        <v-divider class="my-4" />
+                    </div>
+                    <carousel-description />
+                </v-col>
+                <v-col cols="12" lg="6" class="pt-5 pt-lg-0 pl-lg-5">
+                    <div class="login-content pa-lg-5 pt-lg-8">
+                        <div class="inputs mb-8">
+                            <div class="d-none d-lg-block">
+                                <h1 class="login-title">Inicio de sesión</h1>
+                                <v-divider class="my-4" />
+                            </div>
+                            <v-form ref="loginForm" lazy-validation @submit.prevent="login()">
+                                <div class="form-group">
+                                    <label class="black--text">{{ $t("email_address") }}</label>
+                                    <custom-input
+                                        v-model="form.email"
+                                        type="text"
+                                        :error-messages="emailErrors"
+                                        required
+                                    />
+                                </div>
+                                <div class="form-group">
+                                    <label class="black--text">{{ $t("password") }}</label>
+                                    <custom-input
+                                        v-model="form.password"
+                                        type="password"
+                                        :error-messages="passwordErrors"
+                                        required
+                                    />
+                                </div>
+                                <router-link :to="{ name: 'ForgotPassword' }" class="black--text link-custom">
+                                    {{ $t("forgot_password") }}?
+                                </router-link>
+                                <custom-button
+                                    :disabled="loading"
+                                    :loading="loading"
+                                    :text="$t('login')"
+                                    block
+                                    class="mt-3 mb-3"
+                                    color="#888989"
+                                    type="submit"
+                                    @click="login"
+                                />
+                            </v-form>
+                            <router-link :to="{ name: 'Registration' }" class="black--text link-custom">
+                                ¿No tienes ninguna cuenta?
+                            </router-link>
+                            <custom-button
+                                block
+                                light
+                                color="#DFDFDF"
+                                class="mt-3"
+                                text="Registrarse"
+                                :to="{ name: 'Registration' }"
+                            />
+                        </div>
+                        <auth-footer />
+                    </div>
                 </v-col>
             </v-row>
         </v-container>
@@ -11,8 +71,166 @@
 </template>
 
 <script>
-import LoginForm from '../../components/auth/LoginForm.vue';
+import CarouselDescription from "../../components/global/CarouselDescription.vue";
+import CustomButton from "../../components/global/CustomButton.vue";
+import CustomInput from "../../components/global/CustomInput.vue";
+import LayoutNavbarAuth from "../../components/global/LayoutNavbarAuth.vue";
+import AuthFooter from "./AuthFooter.vue";
+
+import { required, email } from "vuelidate/lib/validators";
+import { mapActions, mapGetters, mapMutations } from "vuex";
+
 export default {
-    components: { LoginForm },
+    components: {
+        CarouselDescription,
+        CustomButton,
+        CustomInput,
+        LayoutNavbarAuth,
+        AuthFooter
+    },
+    data: () => ({
+        form: {
+            email: "",
+            password: ""
+        },
+        loading: false
+    }),
+    validations: {
+        form: {
+            email: {
+                required,
+                email
+            },
+            password: {
+                required
+            }
+        }
+    },
+    computed: {
+        ...mapGetters("app", ["generalSettings"]),
+        ...mapGetters("cart", ["getTempUserId"]),
+        ...mapGetters("auth", ["authSettings"]),
+        emailErrors() {
+            const errors = [];
+            if (!this.$v.form.email.$dirty) return errors;
+            !this.$v.form.email.required && errors.push(this.$i18n.t("this_field_is_required"));
+            !this.$v.form.email.email && errors.push(this.$i18n.t("this_field_is_required_a_valid_email"));
+            return errors;
+        },
+        passwordErrors() {
+            const errors = [];
+            if (!this.$v.form.password.$dirty) return errors;
+            !this.$v.form.password.required && errors.push(this.$i18n.t("this_field_is_required"));
+            return errors;
+        }
+    },
+    methods: {
+        ...mapActions("auth", { actionLogin: "login" }),
+        ...mapActions("app", ["fetchProductQuerries"]),
+        ...mapActions("wishlist", ["fetchWislistProducts"]),
+        ...mapActions("cart", ["fetchCartProducts"]),
+        ...mapMutations("cart", ["removeTempUserId"]),
+        ...mapMutations("auth", ["updateChatWindow", "showLoginDialog"]),
+        async login() {
+            this.$v.form.$touch();
+            if (this.$v.form.$anyError) {
+                return;
+            }
+            if (this.getTempUserId) {
+                this.form.temp_user_id = this.getTempUserId;
+            }
+            this.loading = true;
+            const res = await this.call_api("post", "auth/login", this.form);
+            if (res.data.success) {
+                if (res.data.verified == true || this.authSettings.customer_otp_with == "disabled") {
+                    if (this.getTempUserId) {
+                        this.removeTempUserId();
+                    }
+                    this.actionLogin(res.data);
+                    this.showLoginDialog(false);
+                    this.updateChatWindow(false);
+                    this.fetchWislistProducts();
+                    this.fetchProductQuerries();
+                    this.fetchCartProducts();
+                    this.$router.push(this.$route.query.redirect || { name: "DashBoard" });
+                } else {
+                    if (this.authSettings.customer_login_with == "email") {
+                        this.$router.push({
+                            name: "VerifyAccount",
+                            params: { email: this.form.email }
+                        });
+                    }
+                }
+
+                this.snack({
+                    message: res.data.message
+                });
+            } else {
+                this.snack({
+                    message: res.data.message,
+                    color: "red"
+                });
+            }
+            this.loading = false;
+        }
+    }
 };
 </script>
+
+<style lang="scss">
+.v-application {
+    &.theme--light {
+        background: #dee0e0;
+    }
+}
+</style>
+
+<style lang="scss" scoped>
+.login {
+    height: 100%;
+
+    &-title {
+        font-size: 24px;
+        font-weight: 600;
+        letter-spacing: 0;
+        line-height: 30px;
+    }
+
+    &-content {
+        @media (min-width: 1264px) {
+            border: 1px solid #e4e4e4;
+            border-radius: 10px;
+        }
+
+        height: 100%;
+        display: flex;
+        flex-direction: column;
+        justify-content: space-between;
+    }
+}
+
+.wrap {
+    background-color: #fafcfc;
+    border-radius: 10px;
+}
+
+.v-divider {
+    border-color: #e4e4e4 !important;
+}
+
+.form-group {
+    label {
+        font-family: "Overpass", sans-serif;
+        font-size: 12px;
+        letter-spacing: 0.4px;
+        text-transform: uppercase;
+    }
+}
+
+.link-custom {
+    font-family: "Overpass", sans-serif;
+    font-size: 12px;
+    letter-spacing: 0.4px;
+    text-transform: uppercase;
+}
+</style>
--- resources/js/router/auth.js
+++ resources/js/router/auth.js
@@ -1,10 +1,7 @@
 let Login = () => import("../pages/auth/Login");
-let Login2 = () => import("../pages/auth/Login2");
 let Registration = () => import("../pages/auth/Registration");
-let Registration2 = () => import("../pages/auth/Registration2");
 let RegistrationSuccess = () => import("../pages/auth/RegistrationSuccess");
 let ForgotPassword = () => import("../pages/auth/ForgotPassword");
-let ForgotPassword2 = () => import("../pages/auth/ForgotPassword2");
 let NewPassword = () => import("../pages/auth/NewPassword");
 let VerifyAccount = () => import("../pages/auth/VerifyAccount");
 
@@ -16,21 +13,9 @@ export default [
         meta: { requiresAuth: false }
     },
     {
-        path: "/user/login2",
-        component: Login2,
-        name: "Login2",
-        meta: { requiresAuth: false }
-    },
-    {
         path: "/user/registration",
         component: Registration,
         name: "Registration",
-        meta: { requiresAuth: false }
-    },
-    {
-        path: "/user/registration2",
-        component: Registration2,
-        name: "Registration2",
         meta: { requiresAuth: false }
     },
     {
@@ -43,12 +28,6 @@ export default [
         path: "/user/forgot-password",
         component: ForgotPassword,
         name: "ForgotPassword",
-        meta: { requiresAuth: false }
-    },
-    {
-        path: "/user/forgot-password2",
-        component: ForgotPassword2,
-        name: "ForgotPassword2",
         meta: { requiresAuth: false }
     },
     {
