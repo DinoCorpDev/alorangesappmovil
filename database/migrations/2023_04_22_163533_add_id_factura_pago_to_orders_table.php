c8d9fcf341966c05a70ee80f19361b143527d594
Jhon Michael Garcia <me@maikoldev.com>
2023-04-22 19:01:10 -0500

    Fix migration

--- database/migrations/2023_04_22_163533_add_id_factura_pago_to_orders_table.php
+++ database/migrations/2023_04_22_163533_add_id_factura_pago_to_orders_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class AddFieldsToAddressesTable extends Migration
+class AddIdFacturaPagoToOrdersTable extends Migration
 {
     /**
      * Run the migrations.
@@ -14,7 +14,7 @@ class AddFieldsToAddressesTable extends Migration
     public function up()
     {
         Schema::table('orders', function (Blueprint $table) {
-            $table->int('id_factura_pago')->after('tracking_url');
+            $table->integer('id_factura_pago')->after('tracking_url');
         });
     }
 
