f29b10845d4da5353c1175d3b6b8439ed8538b5b
Luis Ugueto <ugueto.luis19@gmail.com>
2023-09-17 09:29:34 -0400

    add modal

---
+++ resources/js/components/auth/Modal.vue
@@ -0,0 +1,64 @@
+<template>
+    <v-dialog
+      v-model="showLogin"
+      width="auto"
+    >
+      <template v-slot:activator="{ props }">
+        <v-btn
+          color="primary"
+          v-bind="props"
+        >
+          Open Dialog
+        </v-btn>
+      </template>
+
+      <v-card>
+        <v-card-text>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+        </v-card-text>
+        <v-card-actions>
+          <v-btn color="primary" block @click="dialog = false">Close Dialog</v-btn>
+        </v-card-actions>
+      </v-card>
+    </v-dialog>
+</template>
+
+<script>
+import { required, email } from "vuelidate/lib/validators";
+import { mapActions, mapGetters, mapMutations } from "vuex";
+
+
+
+export default {
+    components: {
+        
+    },
+    data: () => ({
+        loading: false,
+        showLogin: false,
+       
+    }),
+    validations: {
+        form: {
+            email: {
+                required,
+                email
+            },
+            password: {
+                required
+            }
+        }
+    },
+    computed: {
+
+    },
+    mounted() {
+        this.$vuetify.theme.dark = false;
+    },
+    methods: {
+        async showModal() {
+            return this.showLogin = true;
+        }
+    }
+};
+</script>
