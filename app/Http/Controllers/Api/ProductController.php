af593b5fa9e32f77c5c0c28d6f0647dcc124587a
BrayanStyd11 <brayantriana22@gmail.com>
2024-09-11 10:10:40 -0500

    Archivos de productos recuperado

--- app/Http/Controllers/Api/ProductController.php
+++ app/Http/Controllers/Api/ProductController.php
@@ -292,10 +292,6 @@ class ProductController extends Controller
     }
 
     public function alegra(Request $request){
-<<<<<<< HEAD
-=======
-
->>>>>>> aea77a7 (Conflicto de ramas arreglado)
         $products = [];
         $counter = 0;
         $categoryId = 0;
