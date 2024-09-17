38ff67cf740487ba866537fe35e6c269ef8b70ca
BrayanStyd11 <brayantriana22@gmail.com>
2024-09-17 12:53:37 -0500

    Configuración para traer consulta de alegra y clasificarla de forma correcta

--- app/Models/ProductCategory.php
+++ app/Models/ProductCategory.php
@@ -6,7 +6,10 @@ use Illuminate\Database\Eloquent\Model;
 
 class ProductCategory extends Model
 {
-    protected $guarded = [];
+    protected $table = 'product_categories';
+    protected $guarded = ['id'];
+    protected $fillable = ['product_id','category_id','created_at','updated_at','deleted_at'];
+
 
     public function product()
     {
--- resources/js/pages/shop/ShopPapeleria.vue
+++ resources/js/pages/shop/ShopPapeleria.vue
@@ -1,5 +1,5 @@
 <template>
-    <ShopShowApi category="PAPELERIA"/>
+    <ShopShowApi category="Papeleria2"/>
 </template>
 
 <script>
