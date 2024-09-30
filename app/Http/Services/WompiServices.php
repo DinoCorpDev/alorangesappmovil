190f9dbf3160f9a52577427a13ef91e555316128
BrayanStyd11 <brayantriana22@gmail.com>
2024-09-30 18:35:31 -0500

    Configuración de token de aceptación, tokenización de la tarjeta y metodo de pago de wompi

---
+++ app/Http/Services/WompiServices.php
@@ -0,0 +1,76 @@
+<?php
+
+namespace App\Http\Services;
+
+use Exception;
+use Illuminate\Support\Facades\Http;
+use GuzzleHttp\Client;
+
+class WompiServices{
+    private $client;
+    private $options;
+
+    public function __construct(){
+        $this->client = new Client(['verify' => false ]);
+        $this->options = [
+            'headers' => [
+                'accept' => 'application/json',
+            ],
+        ];
+        $this->url = 'https://sandbox.wompi.co/v1/';
+        $this->token_pub_key = 'pub_test_0HFZFgu0zGNrczp6mTp0vtuuosqQjf8l';
+
+        $this->postHeaders = [
+            'accept' => '/',
+            'Authorization'=> 'Bearer '.$this->token_pub_key,
+            'Content-Type' => 'application/json',
+        ];
+    }
+
+    public function getAceptanceToken(){
+        $responseService = $this->client->request('GET', $this->url.'merchants/'.$this->token_pub_key, $this->options);
+        $response = json_decode($responseService->getBody(), true);
+        foreach ($response as $key => $res) {
+            if (isset($res['presigned_acceptance'])) {
+                $data = [
+                    'presigned_acceptance' => $res['presigned_acceptance'],
+                ];
+                return $data;
+            }
+        }
+    }
+
+    public function tokenizeCard($cardData){
+        try {
+            $response = $this->client->request('POST', $this->url.'tokens/cards', [
+                'headers' => $this->postHeaders,
+                'json' => $cardData
+            ]);
+
+            $res = $response = json_decode($response->getBody()->getContents(), true);;
+
+            return $res;
+        } catch (\Exception $e) {
+            // Manejar el error
+            echo 'Error: ' . $e->getMessage();
+        }
+
+        return $response;
+    }
+
+    public function wompiTransaction($paymentInformation){
+        try {
+            $response = $this->client->request('POST', $this->url.'transactions', [
+                'headers' => $this->postHeaders,
+                'json' => $paymentInformation
+            ]);
+
+            $res = json_decode($response->getBody()->getContents(), true);
+
+            return $res;
+        } catch (\Exception $e) {
+            // Manejar el error
+            return $e->getMessage();
+        }
+    }
+}
