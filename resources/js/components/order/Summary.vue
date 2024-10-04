7b5415579f61c2323d1ef55374f7961074e42109
BrayanStyd11 <brayantriana22@gmail.com>
2024-10-04 11:42:39 -0500

    Configuración de facturación

--- resources/js/components/order/Summary.vue
+++ resources/js/components/order/Summary.vue
@@ -203,7 +203,7 @@ export default {
     },
     methods: {
         iterateOrderUpdates() {
-            const orderUpdates = this.orderDetails.order_updates;
+            const orderUpdates = this.orderDetails.orders;
             this.orderDetails.orders[0].order_updates = orderUpdates;
         }
     }
